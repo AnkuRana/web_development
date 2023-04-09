2e8c54beda5674f324a03df742d23109f5f2034c
AnkuRana <amitrana.com0007@gmail.com>
2023-04-09 19:18:26 +0530

    java script drum kit challenge

---
+++ javascript/drum_kit/index.js
@@ -0,0 +1,31 @@
+var buttons = document.querySelectorAll(".drum")
+for (let index = 0; index < buttons.length; index++) {
+    buttons[index].addEventListener("click", function() {
+        this.style.color = "white";
+        switch(this.innerHTML){
+            case 'w':
+                new Audio("./sounds/tom-1.mp3").play();            
+                break;
+            case 'a':
+                new Audio("./sounds/tom-2.mp3").play();
+                break;
+            case 's':
+                new Audio("./sounds/tom-3.mp3").play();
+                break;
+            case 'd':
+                new Audio("./sounds/tom-4.mp3").play();
+                break;
+            case 'j':
+                new Audio("./sounds/snare.mp3").play();
+                break;
+            case 'k':
+                new Audio("./sounds/crash.mp3").play();
+                break;
+            case 'l':
+                new Audio("./sounds/kick-bass.mp3").play();
+                break;
+            default:
+                console.log("logging default response");
+        }
+    });
+}
